805d97ffd69fb37f0839bb3d28c37254ac0edb00
luisgustavo <luis.gustav0@live.com>
2018-10-21 11:32:27 -0300

    primeiro commit

--- app6/src/componentes/ComunicacaoIndireta.js
+++ app6/src/componentes/ComunicacaoIndireta.js
@@ -8,9 +8,11 @@ import styles from '../styles/sistema'
 
 export const Entrada = props => 
     <View>
-        <TextInput value={props.texto}
+        <TextInput 
+            value={props.texto}
             style={styles.input}
-            onChangeText={props.chamarQuandoMudar} />
+            onChangeText={props.chamarQuandoMudar} 
+        />
     </View>
 
 export default class TextoSincronizado extends Component {
@@ -25,9 +27,14 @@ export default class TextoSincronizado extends Component {
     render() {
         return (
             <View>
-                <Text style={styles.fonte40}>{this.state.texto}</Text>
-                <Entrada texto={this.state.texto}
-                    chamarQuandoMudar={this.alterarTexto} />
+                <Text style={styles.fonte40}>
+                    {this.state.texto}
+                </Text>
+                
+                <Entrada 
+                    texto={this.state.texto}
+                    chamarQuandoMudar={this.alterarTexto} 
+                />
             </View>
         )
     }
